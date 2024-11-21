490a14cb01f5e765f0f18e7d75d11401e5530ad7
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-21 16:49:23 +0700

    menambah val untul nim, email, notelepon

--- app/src/main/java/com/example/simpleviewmodel/model/Mahasiswa.kt
+++ app/src/main/java/com/example/simpleviewmodel/model/Mahasiswa.kt
@@ -2,6 +2,9 @@ package com.example.simpleviewmodel.model
 
 data class Mahasiswa(
     val nama: String = "",
+    val nim: String = "",
+    val email: String = "",
     val gender: String = "",
-    val alamat: String = "",
+    val noTelepon: String = "",
+    val alamat: String = ""
 )
