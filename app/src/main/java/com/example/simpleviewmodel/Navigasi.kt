0df5884dba5d97e356acf8f4309cfdd04f20a708
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-21 12:48:37 +0700

    Menambahkan fungsi Navigasi dengan ViewModel dan NavHostController

--- app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
@@ -1,6 +1,20 @@
 package com.example.simpleviewmodel
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.simpleviewmodel.viewmodel.MahasiswaViewModel
+
 enum class Halaman{
     Form,
     Data
 }
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel= viewModel(),
+    navHost: NavHostController = rememberNavController()
+){}
